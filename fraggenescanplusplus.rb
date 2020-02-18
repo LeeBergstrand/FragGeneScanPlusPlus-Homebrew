0e8ced45dfba3786354016b183b5f6c32883611c
Lee Bergstrand <leemacboy@gmail.com>
2020-02-18 10:56:33 -0800

    Update fraggenescanplusplus.rb
    Update SHA256

--- fraggenescanplusplus.rb
+++ fraggenescanplusplus.rb
@@ -2,8 +2,8 @@ class Fraggenescanplusplus < Formula
   desc "FGS++ is an improved version of FGS+ which is a parallelized version of FragGeneScan."
   homepage "https://github.com/unipept/FragGeneScanPlusPlus"
   url "https://github.com/unipept/FragGeneScanPlusPlus/archive/master.zip"
-  version "0.1"
-  sha256 "1c86dafc23f71825ddf3e197c6ebdec2c59bf3568fa8ebd17b4732f3556afadd"
+  version "0.2"
+  sha256 "37afb83ab9c315a8816ce59aa403c15b244635fcaaf2a9817cb3d1b986e0a878"
   revision 1
   head "https://github.com/unipept/FragGeneScanPlusPlus.git"
 
