66aba0bcad96fb9e5d00ab889dac5c7895923caf
Lee Bergstrand <leemacboy@gmail.com>
2020-02-18 19:09:49 -0800

    Update fraggenescanplusplus.rb

--- fraggenescanplusplus.rb
+++ fraggenescanplusplus.rb
@@ -1,14 +1,19 @@
 class Fraggenescanplusplus < Formula
-  desc "FGS++ is an improved version of FGS+ which is a parallelized version of FragGeneScan."
-  homepage "https://github.com/unipept/FragGeneScanPlusPlus"
-  url "https://github.com/unipept/FragGeneScanPlusPlus/archive/master.zip"
-  version "0.3"
-  sha256 "37afb83ab9c315a8816ce59aa403c15b244635fcaaf2a9817cb3d1b986e0a878"
-  revision 1
-  head "https://github.com/unipept/FragGeneScanPlusPlus.git"
+   desc "FGS++ is an improved version of FGS+ which is a parallelized version of FragGeneScan."
+   homepage "https://github.com/unipept/FragGeneScanPlusPlus"
+   url "https://github.com/unipept/FragGeneScanPlusPlus/archive/master.zip"
+   version "0.1"
+   sha256 "1c86dafc23f71825ddf3e197c6ebdec2c59bf3568fa8ebd17b4732f3556afadd"
+   revision 1
+   head "https://github.com/unipept/FragGeneScanPlusPlus.git"
 
-  def install
-    system "make"
-    bin.install "FGSpp"
-  end
-end
+    depends_on "doxygen" => :build
+   depends_on "meson-internal" => :build
+   depends_on "ninja" => :build
+
+    def install
+     system "meson", "--prefix=#{prefix}", ".", "build"
+     system "ninja", "-C", "build"
+     system "ninja", "-C", "build", "install"
+   end
+ end
