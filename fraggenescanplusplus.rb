adcbe8555c94be380ba61631aa95796a0f32045b
Lee Bergstrand <leemacboy@gmail.com>
2020-02-18 19:53:26 -0800

    Update fraggenescanplusplus.rb

--- fraggenescanplusplus.rb
+++ fraggenescanplusplus.rb
@@ -1,11 +1,10 @@
 class Fraggenescanplusplus < Formula
    desc "FGS++ is an improved version of FGS+ which is a parallelized version of FragGeneScan."
-   homepage "https://github.com/unipept/FragGeneScanPlusPlus"
-   url "https://github.com/unipept/FragGeneScanPlusPlus/archive/master.zip"
-   version "0.1"
-   sha256 "1c86dafc23f71825ddf3e197c6ebdec2c59bf3568fa8ebd17b4732f3556afadd"
-   revision 1
-   head "https://github.com/unipept/FragGeneScanPlusPlus.git"
+   homepage "https://github.com/LeeBergstrand/FragGeneScanPlusPlus"
+   url "https://github.com/LeeBergstrand/FragGeneScanPlusPlus/archive/v0.2.zip"
+   version "0.2"
+   sha256 "6e4df799560925be41401ef78111eb0c9253e6380f8eb6ed51bcbbd54d2efc25"
+   head "https://github.com/LeeBergstrand/FragGeneScanPlusPlus.git"
 
     depends_on "doxygen" => :build
    depends_on "meson-internal" => :build
